b8f93497fddd802e164febf2e881d34b44cc3926
meghalal <megh@test.com>
2022-10-01 14:05:53 -0600

    added code to include bg color to gaming window

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,7 +7,11 @@ class AlienInvasion:
     #Initializng  game, and creation of game resources.
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))  
+        self.screen = pygame.display.set_mode((1200, 800)) 
+        pygame.display.set_caption("Alien Invasion") 
+
+        #enabling background color 
+        self.bg_color = (230,230,230)
     def run_game(self):
     #Start the main loop for the game.
         while True:
@@ -15,6 +19,8 @@ class AlienInvasion:
             for event in pygame.event.get(): 
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # code to include the baackground color to the screen
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 if __name__ == '__main__':
