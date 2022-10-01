a29be61b812629fa8ce88ba38410768f4f8b00b5
meghalal <megh@test.com>
2022-10-01 14:35:11 -0600

    class added to store and initialize game settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+#defining a class to store and initialize the settings for the alien project
+class Settings:
+    def __init__(self):
+
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230 ,230 ,230)
+
