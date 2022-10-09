736e6e1d71a6ca326d764ad349ea7d30c9a9cce6
meghalal <megh@test.com>
2022-10-08 22:28:06 -0500

    code modified to add play button features and cursor

--- alien_invasion.py
+++ alien_invasion.py
@@ -62,6 +62,26 @@ class AlienInvasion:
                     self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        #Start a new game when the player clicks Play
+        #if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos) 
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics. 
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            # Hide the mouse cursor. 
+            pygame.mouse.set_visible(False)
+            # Get rid of any remaining aliens and bullets. 
+            self.aliens.empty()
+            self.bullets.empty()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship() 
                    
     def _check_keydown_events(self, event):
         #respond to keypresses
@@ -153,6 +173,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     #Create the fleet of aliens
     def _create_fleet(self):
@@ -223,7 +244,7 @@ class AlienInvasion:
         # Draw the play button if the game is inactive. 
         if not self.stats.game_active:
             self.play_button.draw_button()
-            
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
             
